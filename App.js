d2e6220f37a1427e15386b697a994959013937db
kozakevich13 <hanibalsmitti@gmail.com>
2023-04-15 10:47:51 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {useState} from 'react'
 
 export default function App() {
+  let [value, setValue] = useState('доров1')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your ass!</Text>
+            <Text style={styles.suka}>{value}</Text>
+            <TextInput
+                style={styles.input}
+                value={value}
+                onChangeText={setValue}
+            ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+    suka: {
+      backgroundColor: 'green',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+  input: {
+      backgroundColor: '#993300',
+      justifyContent: 'center',
+      width: 200,
+      height: 50,
+      borderWidth: 2,
+      borderColor: 'green'
+
+    },
 });
