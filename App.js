19118efd6d7f6c9ead66e59c3415e176ef10f3d2
kozakevich13 <hanibalsmitti@gmail.com>
2023-04-15 14:30:32 +0300

    add main functional todo list
    add local storage

--- App.js
+++ App.js
@@ -1,45 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import {useState} from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  let [value, setValue] = useState('доров1')
+const STORAGE_KEY = '@todos';
+
+
+const TodoList = () => {
+  const [textInput, setTextInput] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [mainCounter, setMainCounter] = useState(0)
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const addTodo = () => {
+    setTodos([...todos, textInput]);
+    setTextInput('');
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+    setMainCounter(mainCounter + 1)
+  };
+
+  const saveTodos = async () => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadTodos = async () => {
+    try {
+      const todosFromStorage = await AsyncStorage.getItem(STORAGE_KEY);
+      if (todosFromStorage !== null) {
+        setTodos(JSON.parse(todosFromStorage));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    saveTodos();
+  }, [todos]);
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your ass!</Text>
-            <Text style={styles.suka}>{value}</Text>
-            <TextInput
-                style={styles.input}
-                value={value}
-                onChangeText={setValue}
-            ></TextInput>
-      <StatusBar style="auto" />
+      <Text style={styles.textTack}>all tasks completed: {mainCounter}</Text>
+      <TouchableOpacity style={styles.button} onPress={addTodo}>
+        <Text>add task</Text>
+      </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        placeholder="Add a todo"
+        value={textInput}
+        onChangeText={(text) => setTextInput(text)}
+      />
+      {todos.map((todo, index) => (
+        <TouchableOpacity key={index} onPress={() => deleteTodo(index)}>
+          <Text style={styles.todo}>{todo}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
-
-    suka: {
-      backgroundColor: 'green',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-
+ 
   input: {
-      backgroundColor: '#993300',
-      justifyContent: 'center',
-      width: 200,
-      height: 50,
-      borderWidth: 2,
-      borderColor: 'green'
-
-    },
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+  },
+  todo: {
+    fontSize: 20,
+    marginBottom: 10,
+    borderColor: '#537075',
+    padding: 2,
+    borderWidth: 2,
+    backgroundColor: '#728f9f',
+    width: 200
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin: 10
+  },
+  textTack: {
+    marginBottom: 20
+  },
 });
+
+export default TodoList;
